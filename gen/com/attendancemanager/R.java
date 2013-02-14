859061fc2b0ee93c8c3904020cb934fb0237d545
Kushal Bhabra <kushalbhabra@gmail.com>
2013-02-14 21:55:38 +0530

    SubjectSelection UI done

--- gen/com/attendancemanager/R.java
+++ gen/com/attendancemanager/R.java
@@ -120,14 +120,25 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btn_plus=0x7f070007;
+        public static final int et_subject=0x7f070006;
         public static final int fixed_tabs=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int frameLayout1=0x7f070005;
+        public static final int hori_divider=0x7f070008;
+        public static final int listView1=0x7f070009;
+        public static final int list_lectures=0x7f070002;
+        public static final int menu_settings=0x7f07000a;
         public static final int pager=0x7f070001;
+        public static final int tv_subjectname=0x7f070003;
+        public static final int tv_time=0x7f070004;
     }
     public static final class layout {
         public static final int activity_feed_timetable=0x7f030000;
         public static final int activity_fixed_tabs=0x7f030001;
-        public static final int tab_fixed=0x7f030002;
+        public static final int edittimetablelist=0x7f030002;
+        public static final int lectureinlist=0x7f030003;
+        public static final int subjectdialog=0x7f030004;
+        public static final int tab_fixed=0x7f030005;
     }
     public static final class menu {
         public static final int activity_feed_timetable=0x7f060000;
@@ -136,6 +147,7 @@ containing a value of this type.
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int subject_hint=0x7f040003;
     }
     public static final class style {
         /** 
